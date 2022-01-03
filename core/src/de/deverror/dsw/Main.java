c0d59debf130609478cfc48ae654c7d9d8c80d47
BuildTools <unconfigured@null.spigotmc.org>
2022-01-03 20:41:14 +0100

    Initial structure

--- core/src/de/deverror/dsw/Main.java
+++ core/src/de/deverror/dsw/Main.java
@@ -1,11 +1,11 @@
 package de.deverror.dsw;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Main extends ApplicationAdapter {
+public class Main extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
--- core/src/de/deverror/dsw/Map.java
+++
@@ -1,9 +0,0 @@
-package de.deverror.dsw;
-
-import com.badlogic.gdx.maps.tiled.TiledMap;
-
-import java.util.ArrayList;
-
-public class Map {
-
-}
